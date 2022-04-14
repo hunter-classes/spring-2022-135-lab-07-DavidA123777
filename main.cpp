24377d527b5ed3c95f84b67fad72590f9abfd370
Dulina <dulinagunasekara@gmail.com>
2022-04-14 09:49:20 -0400

    reverted back

--- main.cpp
+++ main.cpp
@@ -11,45 +11,28 @@ std::fstream newfile;
 newfile.open("new-code.cpp", std::ios::out);
 
 
-
 if (myfile.is_open()){
   std::string line;
-
+  int tab = 0;
   std::string temp = "";
 
   while (getline(myfile, line)){
-    std::cout << line << std::endl;}
-  }
-  myfile.close();
-  myfile.open("bad-code.cpp", std::ios::in);
 
 
-
-
-
-int tab = 0;
-if (myfile.is_open()){
-  std::string line;
-
-  std::string temp = "";
-
-  while (getline(myfile, line)){
-  temp = removeLeadingSpaces(line);
-    if (tab > 0){
-    for (int x = 0; x <= tab; x++){
-      temp = '\t' + temp;
-    }
-    }
-    newfile << temp << endl;
   int s = countChar(line);
 
   tab += s;
 
 
 
+  temp = removeLeadingSpaces(line);
 
 
-
+  if (tab > 0){
+  for (int x = 0; x < tab; x++){
+    temp = '\t' + temp;
+  }
+}
 
 newfile << temp << endl;
 
